937def83e402b63622ca9080354d5c993f549de6
phiman <phiman@thoughtworks.com>
2019-03-11 00:02:05 +0530

    [Himanshu] Removed tests

--- test/TestWallet.sol
+++ test/TestWallet.sol
@@ -6,16 +6,4 @@ import "../contracts/Wallet.sol";
 
 contract TestWallet {
 
-  function testInitialAmountShouldBe20k() public {
-    Wallet wall = new Wallet();
-    uint expected = 20000;
-    Assert.equal(wall.getAmountValue(), expected, "Wallet should be 20000 initially");
-  }
-
-  function testShouldRemove100Successfully() public{
-    Wallet wall = new Wallet();
-    wall.withdrawAmount(100);
-    Assert.equal(wall.getAmountValue(), 19900, "Wallet should be 19900");
-  }
-
 }
